f1794444633e007eff7e05e826a765c1c1ab683c
ANAS BELHACHMI <51194911+u570960@users.noreply.github.com>
2022-05-09 22:36:18 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+class Phrase:
+    def __init__ (self,phrase):
+        self.mots = phrase.split()
+        
+    def upper(self):
+        self.mots = [m.upper() for m in self.mots]
+    
+    def __str__(self):
+        return "\n".join(self.mots)
+
+if test1: 
+	<bloc 1>
+elif test2:
+	<bloc2>
+else:
+	<blocN>
+while a: 
+	a.pop()
+	print(a)
+	
+while True:
+	s = input('Quelle est votre question ? \n')
+	if 'aucune' in s:
+		break
+	
